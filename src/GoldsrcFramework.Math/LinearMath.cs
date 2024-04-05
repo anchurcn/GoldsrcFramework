23eb502e918faf248eafcb1502727283d23a6ff3
anchurcn <809595156@qq.com>
2024-04-06 07:43:42 +0800

    Add csproj Math

---
+++ src/GoldsrcFramework.Math/LinearMath.cs
@@ -0,0 +1,47 @@
+﻿namespace GoldsrcFramework.Math
+{
+    public struct Matrix3x4f
+    {
+        public float M11;
+        public float M12;
+        public float M13;
+        public float M14;
+        public float M21;
+        public float M22;
+        public float M23;
+        public float M24;
+        public float M31;
+        public float M32;
+        public float M33;
+        public float M34;
+    }
+
+    public struct Vector3f
+    {
+        public float X;
+        public float Y;
+        public float Z;
+    }
+
+    public struct Vector4f
+    {
+        public float X;
+        public float Y;
+        public float Z;
+        public float W;
+    }
+
+    public struct Vector2f
+    {
+        public float X;
+        public float Y;
+    }
+
+    public struct Quaternion
+    {
+        public float X;
+        public float Y;
+        public float Z;
+        public float W;
+    }
+}
