586a4a5b4b44563467d37260974b29c3d0ec17dd
anchurcn <809595156@qq.com>
2025-11-07 21:10:13 +0800

    static StudioCalcBoneAdj

--- src/GoldsrcFramework/Rendering/StudioModelRendererEx.cs
+++ src/GoldsrcFramework/Rendering/StudioModelRendererEx.cs
@@ -40,4 +40,69 @@ namespace GoldsrcFramework.Rendering
             return (mstudioanim_t*)((byte*)paSequences[pseqdesc->seqgroup].data + pseqdesc->animindex);
         }
     }
-}
+
+    void CStudioModelRenderer::StudioCalcBoneAdj(float dadt, float* adj, const byte* pcontroller1, const byte* pcontroller2, byte mouthopen)
+        {
+            int i, j;
+            float value;
+            mstudiobonecontroller_t* pbonecontroller;
+
+            pbonecontroller = (mstudiobonecontroller_t*)((byte*)m_pStudioHeader + m_pStudioHeader->bonecontrollerindex);
+
+            for (j = 0; j < m_pStudioHeader->numbonecontrollers; j++)
+            {
+                i = pbonecontroller[j].index;
+                if (i <= 3)
+                {
+                    // check for 360% wrapping
+                    if ((pbonecontroller[j].type & STUDIO_RLOOP) != 0)
+                    {
+                        if (abs(pcontroller1[i] - pcontroller2[i]) > 128)
+                        {
+                            int a, b;
+                            a = (pcontroller1[j] + 128) % 256;
+                            b = (pcontroller2[j] + 128) % 256;
+                            value = ((a * dadt) + (b * (1 - dadt)) - 128) * (360.0 / 256.0) + pbonecontroller[j].start;
+                        }
+                        else
+                        {
+                            value = ((pcontroller1[i] * dadt + (pcontroller2[i]) * (1.0 - dadt))) * (360.0 / 256.0) + pbonecontroller[j].start;
+                        }
+                    }
+                    else
+                    {
+                        value = (pcontroller1[i] * dadt + pcontroller2[i] * (1.0 - dadt)) / 255.0;
+                        if (value < 0)
+                            value = 0;
+                        if (value > 1.0)
+                            value = 1.0;
+                        value = (1.0 - value) * pbonecontroller[j].start + value * pbonecontroller[j].end;
+                    }
+                    // Con_DPrintf( "%d %d %f : %f\n", m_pCurrentEntity->curstate.controller[j], m_pCurrentEntity->latched.prevcontroller[j], value, dadt );
+                }
+                else
+                {
+                    value = mouthopen / 64.0;
+                    if (value > 1.0)
+                        value = 1.0;
+                    value = (1.0 - value) * pbonecontroller[j].start + value * pbonecontroller[j].end;
+                    // Con_DPrintf("%d %f\n", mouthopen, value );
+                }
+                switch (pbonecontroller[j].type & STUDIO_TYPES)
+                {
+                    case STUDIO_XR:
+                    case STUDIO_YR:
+                    case STUDIO_ZR:
+                        adj[j] = value * (M_PI / 180.0);
+                        break;
+                    case STUDIO_X:
+                    case STUDIO_Y:
+                    case STUDIO_Z:
+                        adj[j] = value;
+                        break;
+                }
+            }
+        }
+
+
+    }
