d3780b1abf63e7dc47c397606453829be34a9d63
anchurcn <809595156@qq.com>
2026-06-14 11:34:59 +0800

    add StrideCodeOnlyDemo

---
+++ src/StrideCodeOnlyDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using Stride.CommunityToolkit.Bepu;
+using Stride.CommunityToolkit.Engine;
+using Stride.CommunityToolkit.Rendering.ProceduralModels;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+
+using var game = new Game();
+
+game.Run(start: Start);
+
+void Start(Scene rootScene)
+{
+    game.SetupBase3DScene();
+
+    var entity = game.Create3DPrimitive(PrimitiveModelType.Capsule);
+
+    entity.Transform.Position = new Vector3(0, 8, 0);
+
+    entity.Scene = rootScene;
+}
